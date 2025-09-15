67aa5a5b03a4002004ac11b0f21082d72e97e0cd
Иван <Ower-7@yandex.ru>
2025-09-15 19:30:11 +0500

    add

---
+++ src/main/java/ru/utmn/tkachenko/earthquakes/EarthquakesApplication.java
@@ -0,0 +1,13 @@
+package ru.utmn.tkachenko.earthquakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthquakesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/utmn/tkachenko/earthquakes/controller/EarthquakesController.java
@@ -0,0 +1,13 @@
+package ru.utmn.tkachenko.earthquakes.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EarthquakesController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
